7bae6406e6379e1642f4f7a333bd218f1b9c0af4
Mark Dalrymple <markd@bignerdranch.com>
2018-05-25 21:45:13 -0400

    Stubs in text field button

--- CocoaScrollViews/CocoaScrollViews/AppDelegate.m
+++ CocoaScrollViews/CocoaScrollViews/AppDelegate.m
@@ -1,28 +1,17 @@
-//
-//  AppDelegate.m
-//  CocoaScrollViews
-//
-//  Created by markd on 5/25/18.
-//  Copyright © 2018 Borkware. All rights reserved.
-//
-
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
-@property (weak) IBOutlet NSWindow *window;
-@end
+@property(weak) IBOutlet NSWindow *window;
+@end // extension
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
+- (void) applicationDidFinishLaunching: (NSNotification *) aNotification {
+} // applicationDidFinishLaunching
 
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
-}
+- (IBAction) autoexpandingTextField: (NSButton *) sender {
+    NSLog(@"SNORK");
+} // autoexpandingTextField
 
-
-@end
+@end // AppDelegate
