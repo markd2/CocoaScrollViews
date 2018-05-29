309454c7bb79ad7f1e7d7ab75b27fe16a693d382
Mark Dalrymple <markd@bignerdranch.com>
2018-05-29 19:34:14 -0400

    Gets image centering working

--- CocoaScrollViews/CocoaScrollViews/CenteringImageWindowController.m
+++ CocoaScrollViews/CocoaScrollViews/CenteringImageWindowController.m
@@ -3,9 +3,21 @@
 
 @interface CenteringImageWindowController ()
 
+@property(strong) IBOutlet NSImageView *imageView;
+
 @end // extension
 
 @implementation CenteringImageWindowController
+
+// Constraints:
+//   Center the document view horizontally and vertically in the clip view, >= 0
+//   
+//   Add top and leading constraints from document view to the clip view
+//   Add equal-width and height constraints from document view to the clip view,
+//      lower priority (e.g. 400s)
+// Reminder:
+//   * document view is your big view that gets scrolled
+//   * content view is the clip view, what's totally visible.
 
 + (instancetype) newController {
     CenteringImageWindowController *ciwc = 
@@ -17,6 +29,13 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
+
+    NSImage *image = [NSImage imageNamed: @"cantina-band"];
+    self.imageView.image = image;
 } // windowDidLoad
+
+- (IBAction) toggleBiggieSmalls: (NSButton *) sender {
+    NSLog(@"OOGIE");
+} // toggleBiggieSmalls
 
 @end // CenteringImageWindowController
